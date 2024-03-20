23ebba44372a4a5bfa989eae103f20455af46deb
Upaayan Naskar <upaayan@gmail.com>
2024-03-21 00:23:44 +0530

    updating bet_buttons

--- views/bet_buttons.py
+++ views/bet_buttons.py
@@ -7,11 +7,7 @@ import discord
 class homeTeamButton(Button):
     def __init__(self, label : str, disabled : bool, matchID : str, emoji : discord.Emoji):
 
-<<<<<<< HEAD
         super().__init__(style = discord.ButtonStyle.green, label = f"Bet for {label}", disabled = disabled, custom_id = 'homeTeamBtn', row = 1, emoji = emoji)
-=======
-        super().__init__(style = discord.ButtonStyle.green, label = label, disabled = disabled, custom_id = 'homeTeamBtn', row = 1, emoji = emoji)
->>>>>>> caed42f88b689c05a1fd55f92681f52329d71a4f
 
         self.label = label
         self.matchID = matchID
@@ -29,11 +25,7 @@ class homeTeamButton(Button):
 class awayTeamButton(Button):
     def __init__(self, label : str, disabled : bool, matchID: str, emoji : discord.Emoji):
 
-<<<<<<< HEAD
         super().__init__(style = discord.ButtonStyle.green, label = f"Bet for {label}", disabled = disabled, custom_id = 'awayTeamBtn', row = 1, emoji = emoji)
-=======
-        super().__init__(style = discord.ButtonStyle.green, label = label, disabled = disabled, custom_id = 'awayTeamBtn', row = 1, emoji = emoji)
->>>>>>> caed42f88b689c05a1fd55f92681f52329d71a4f
 
         self.label = label
         self.matchID = matchID
